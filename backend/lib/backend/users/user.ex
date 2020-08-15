1fdc99251567644c6936e9644db2b97a321918e3
japerry911 <jack.e.perry@outlook.com>
2020-08-15 15:10:36 -0600

    CRUD user endpoints created

---
+++ backend/lib/backend/users/user.ex
@@ -0,0 +1,18 @@
+defmodule Backend.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :img_url, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :img_url])
+    |> validate_required([:username, :img_url])
+  end
+end
