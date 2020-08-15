dc9dc9f882db47e4839ba1b0252e1de12da4660a
japerry911 <jack.e.perry@outlook.com>
2020-08-15 16:40:54 -0600

    Categories endpoints added and added cat id to topics table, ready-for-merge

---
+++ backend/lib/backend/categories/category.ex
@@ -0,0 +1,19 @@
+defmodule Backend.Categories.Category do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "categories" do
+    field :description, :string
+    field :name, :string
+    has_many :topics, Backend.Topics.Topic
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(category, attrs) do
+    category
+    |> cast(attrs, [:name, :description])
+    |> validate_required([:name, :description])
+  end
+end
