7a0bec3b2a1bf077b58d13d89c9cddbdcfa0fb5a
japerry911 <jack.e.perry@outlook.com>
2020-08-17 13:23:15 -0600

    Show Post endpoint fixed

--- backend/lib/backend/posts.ex
+++ backend/lib/backend/posts.ex
@@ -38,7 +38,11 @@ defmodule Backend.Posts do
       ** (Ecto.NoResultsError)
 
   """
-  def get_post!(id), do: Repo.get!(Post, id)
+  def get_post!(id) do
+    Post
+    |> preload([:user, :topic])
+    |> Repo.get!(id)
+  end
 
   @doc """
   Creates a post.
