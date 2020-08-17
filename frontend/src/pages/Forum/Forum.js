e6ac9b2365292808af2b5fea34d09782a8c7c896
japerry911 <jack.e.perry@outlook.com>
2020-08-17 14:04:54 -0600

    Pages boilerplate set up except for show pages for posts and topics. Router added, and routes for boilerplate pages set up, ready-for-merge

---
+++ frontend/src/pages/Forum/Forum.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useStyles } from "./ForumStyles";
+
+const Forum = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>Forum Page</h1>
+    </div>
+  );
+};
+
+export default Forum;
