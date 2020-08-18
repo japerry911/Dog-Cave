f1e3201599f01388ab3ecb75c6a03e8d8e816b14
japerry911 <jack.e.perry@outlook.com>
2020-08-18 13:35:18 -0600

    progress on making layout, need to add in categories link to EP and put links and stats on right side

--- frontend/src/pages/ForumsMain/ForumsMain.js
+++ frontend/src/pages/ForumsMain/ForumsMain.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from "react";
 import HeroHeader from "../../components/HeroHeader/HeroHeader";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 import { useStyles } from "./ForumsMainStyles";
 
 const ForumsMain = () => {
@@ -7,11 +9,58 @@ const ForumsMain = () => {
 
   return (
     <Fragment>
-      <HeroHeader
-        imgUrl="https://dog-cave2134912939213.s3.us-east-2.amazonaws.com/ForumMain/agustina-heit-tC3hq_XAfv0-unsplash.jpg"
-        headerText="Dog Cave Forums"
-        pushDown
-      />
+      <div className={classes.mainDivStyle}>
+        <HeroHeader
+          imgUrl="https://dog-cave2134912939213.s3.us-east-2.amazonaws.com/ForumMain/agustina-heit-tC3hq_XAfv0-unsplash.jpg"
+          headerText="Dog Cave Forums"
+          pushDown
+        />
+        <Grid container justify="center" className={classes.mainContainerStyle}>
+          <Grid
+            container
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+            align="center"
+            justify="center"
+          >
+            <Paper className={classes.paperStyle}>
+              <Grid
+                item
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+                xl={12}
+                className={classes.gridItemStyle}
+              >
+                <figure className={classes.figureStyle}>
+                  <img
+                    src="https://dog-cave2134912939213.s3.us-east-2.amazonaws.com/Logo/your-logo.png"
+                    alt="Dog Cave logo"
+                    className={classes.logoStyle}
+                  />
+                </figure>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+                xl={12}
+                justify="center"
+              >
+                <Paper className={classes.subPaperStyle}></Paper>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     </Fragment>
   );
 };
--- frontend/src/pages/ForumsMain/ForumsMainStyles.js
+++ frontend/src/pages/ForumsMain/ForumsMainStyles.js
@@ -1,3 +1,57 @@
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
-export const useStyles = makeStyles((theme) => ({}));
+export const useStyles = makeStyles((theme) => ({
+  mainDivStyle: {
+    minHeight: "100vh",
+    width: "100%",
+    backgroundColor: theme.palette.secondary.main,
+    paddingBottom: "3rem",
+    [theme.breakpoints.only("xs")]: {
+      minHeight: "85rem",
+    },
+  },
+  mainContainerStyle: {
+    minHeight: "40rem",
+  },
+  paperStyle: {
+    border: `3pt solid ${theme.palette.secondary.main}`,
+    padding: "1rem 0",
+    marginTop: "3rem",
+    height: "55rem",
+    width: "60%",
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: 12,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  titleStyle: {
+    fontWeight: "bold",
+    color: theme.colors.lightGray,
+  },
+  figureStyle: {
+    width: "100%",
+    marginBlockStart: 0,
+    marginBlockEnd: 0,
+    marginInlineStart: 0,
+    marginInlineEnd: 0,
+  },
+  logoStyle: {
+    width: "100%",
+  },
+  subPaperStyle: {
+    width: "80%",
+    height: "90%",
+    padding: "1rem 0",
+    backgroundColor: theme.colors.lightGray,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 12,
+  },
+  gridItemStyle: {
+    flexBasis: "45%",
+  },
+}));
