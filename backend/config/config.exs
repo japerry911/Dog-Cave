75d72042d49fa56cfa6b9e58a3a048a41aec5018
japerry911 <jack.e.perry@outlook.com>
2020-08-17 23:05:33 -0600

    started on backend smtp integration, bedtime.

--- backend/config/config.exs
+++ backend/config/config.exs
@@ -29,3 +29,12 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :backend, Backend.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  address: "smtp.gmail.com",
+  port: 587,
+  username: System.get_env("SMTP_USERNAME"),
+  password: System.get_env("SMTP_PASSWORD"),
+  authentication: "plain",
+  enable_starttls_auto: true
--- backend/mix.exs
+++ backend/mix.exs
@@ -20,7 +20,7 @@ defmodule Backend.MixProject do
   def application do
     [
       mod: {Backend.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :bamboo]
     ]
   end
 
@@ -42,7 +42,9 @@ defmodule Backend.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:bamboo, "~> 0.7"},
+      {:bamboo_smtp, "~> 1.2.1"}
     ]
   end
 
