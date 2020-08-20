f6c86785f6adac36dc9042d00f2bbdc1ae4e740f
japerry911 <jack.e.perry@outlook.com>
2020-08-20 08:15:22 -0600

    register_changeset created

--- backend/lib/backend/users/user.ex
+++ backend/lib/backend/users/user.ex
@@ -7,6 +7,8 @@ defmodule Backend.Users.User do
   schema "users" do
     field :img_url, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
     has_many :posts, Backend.Posts.Post
     has_many :topics, Backend.Topics.Topic
 
@@ -19,5 +21,25 @@ defmodule Backend.Users.User do
     |> cast(attrs, [:username, :img_url])
     |> validate_required([:username, :img_url])
     |> validate_length(:username, min: 5, max: 20)
+  end
+
+  @doc false
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 40)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
