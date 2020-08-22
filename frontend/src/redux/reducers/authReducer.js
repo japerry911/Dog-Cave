260d58b7939e40d1651b9bca0928653e7194c667
japerry911 <jack.e.perry@outlook.com>
2020-08-21 22:59:00 -0600

    started creating user auth redux reducer and actions

---
+++ frontend/src/redux/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  user: {},
+  authed: false,
+  isLoading: false,
+  error: null,
+  token: null,
+};
+
+const authReducer = (state = INITIAL_STATE, action) => {
+  switch (action.payload) {
+  }
+};
