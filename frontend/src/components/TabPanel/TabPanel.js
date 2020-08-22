8b0136117f4f3a2f5a92765b93c2f8313b0943da
japerry911 <jack.e.perry@outlook.com>
2020-08-22 14:47:01 -0600

    tab panel draft completed

--- frontend/src/components/TabPanel/TabPanel.js
+++ frontend/src/components/TabPanel/TabPanel.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
-const TabPanel = () => <div></div>;
+const TabPanel = ({ children, value, index, ...other }) => (
+  <div
+    role="tabpanel"
+    hidden={value !== index}
+    id={`simple-tabpanel-${index}`}
+    aria-labelledby={`simple-tab-${index}`}
+    {...other}
+  >
+    {value === index && (
+      <Box p={3}>
+        <Typography>{children}</Typography>
+      </Box>
+    )}
+  </div>
+);
 
 export default TabPanel;
