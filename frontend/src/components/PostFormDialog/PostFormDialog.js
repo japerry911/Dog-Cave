f1b6ad2a10c00bfd93668bb9915c26d1f92df455
japerry911 <jack.e.perry@outlook.com>
2020-08-24 11:14:12 -0600

    boilerplate set up for PostFormDialog component

---
+++ frontend/src/components/PostFormDialog/PostFormDialog.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useStyles } from "./PostFormDialogStyles";
+
+const PostFormDialog = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>Post Form Dialog</h1>
+    </div>
+  );
+};
+
+export default PostFormDialog;
